4538200a0a68a2e820a26e133b54fdf0410e5f7f
Atiksha <atiksha@webenertia.com>
2023-10-19 18:47:11 +0530

    Update

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -27,7 +27,7 @@ export default function Page() {
   ];
 
   return (
-    <section className='bg-green pb-48'>
+    <section className='bg-green md:pb-48'>
       <div className="container ">
         <div className="upper-layout">
           <Image className='py-2 mx-auto' src="/live.png" width={300} height={100} />
@@ -35,15 +35,15 @@ export default function Page() {
         <div className="logo-wrap">
           <Image className='mx-auto' src="/sports.jpg" width={1200} height={200} />
         </div>
-        <div className="content py-10 text-center pb-20">
-          <h1 className='text-4xl font-bold mb-3'>Welcome to LiveSportsDuniya!</h1>
-          <p className='text-white text-xl tracking-widest'>Get live updates, scores, and news from your favorite sports</p>
+        <div className="content py-10 text-center pb-10 lg:pg-20">
+          <h1 className=' text-2xl md:text-4xl font-bold mb-3'>Welcome to LiveSportsDuniya!</h1>
+          <p className='text-white text-lg md:text-xl tracking-widest'>Get live updates, scores, and news from your favorite sports</p>
         </div>
         <div className="sports bg-brown mb-20">
-          <ul className='flex flex-wrap py-2 justify-center'>
+          <ul className='flex flex-wrap py-4 justify-center'>
             {sports.map((sport, index) => (
-              <li key={index}>
-                <Link className='text-white text-xl mx-5 py-1' href={sport.src}>
+              <li key={index} className="w-full md:w-auto text-center mb-4 md:mb-0">
+                <Link className='text-white text-xl mx-5 py-1 ' href={sport.src}>
                   {sport.name}
                 </Link>
               </li>
@@ -51,7 +51,7 @@ export default function Page() {
           </ul>
         </div>
       </div>
-      <div className=' bg-white py-20'></div>
+      <div className=' bg-white md:py-20 py-10'></div>
     </section>
   );
 }
